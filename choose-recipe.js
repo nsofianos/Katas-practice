7d01222642980e8f64ca4ca5420b41a6dc1bdb5d
Nik Sofianos <nsofianos@hotmail.com>
2021-10-02 01:04:17 +0000

    loop thru arrays and objects

---
+++ choose-recipe.js
@@ -0,0 +1,57 @@
+const checkIngredients = (bakery, ingredients) => {
+  for (const ing of ingredients) {
+    for (const ingredient of bakery) {
+      if (ing === ingredient) return true;
+    }
+  }
+  return false;
+};
+
+const chooseRecipe = function (bakeryA, bakeryB, recipes) {
+  for (const recipe of recipes) {
+    if (
+      checkIngredients(bakeryA, recipe.ingredients) &&
+      checkIngredients(bakeryB, recipe.ingredients)
+    ) {
+      return recipe.name;
+    }
+  }
+};
+
+let bakeryA = ["saffron", "eggs", "tomato paste", "coconut", "custard"];
+let bakeryB = ["milk", "butter", "cream cheese"];
+let recipes = [
+  {
+    name: "Coconut Sponge Cake",
+    ingredients: ["coconut", "cake base"],
+  },
+  {
+    name: "Persian Cheesecake",
+    ingredients: ["saffron", "cream cheese"],
+  },
+  {
+    name: "Custard Surprise",
+    ingredients: ["custard", "ground beef"],
+  },
+];
+
+console.log(chooseRecipe(bakeryA, bakeryB, recipes));
+
+bakeryA = ["potatoes", "bay leaf", "raisins"];
+bakeryB = ["red bean", "dijon mustard", "apples"];
+recipes = [
+  {
+    name: "Potato Ganache",
+    ingredients: ["potatoes", "chocolate"],
+  },
+  {
+    name: "Sweet Fish",
+    ingredients: ["anchovies", "honey"],
+  },
+  {
+    name: "Nima's Famous Dijon Raisins",
+    ingredients: ["dijon mustard", "raisins"],
+  },
+];
+
+console.log(chooseRecipe(bakeryA, bakeryB, recipes));
